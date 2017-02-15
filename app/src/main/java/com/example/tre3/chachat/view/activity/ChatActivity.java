3d41f4906087ea891ac8745e901a187505937cc4
KeisukeImai <keisuke@KIMac.local>
2017-02-16 01:16:53 +0900

    Create a base of chat view.

--- app/src/main/java/com/example/tre3/chachat/view/activity/ChatActivity.java
+++ app/src/main/java/com/example/tre3/chachat/view/activity/ChatActivity.java
@@ -2,10 +2,21 @@ package com.example.tre3.chachat.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class ChatActivity {
+import com.example.tre3.chachat.R;
+
+public class ChatActivity extends AppCompatActivity {
 
     public static Intent createIntent(Context context) {
         return new Intent(context, ChatActivity.class);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
     }
 }
